1ff0c4e8dfecae6bf5aad5149e3a622f28d4da14
Michael Edlund <medlund@mac.com>
2026-02-28 17:21:17 -0800

    Another attempt to fix broken generator templates

--- lib/generators/overture_maps/install_generator.rb
+++ lib/generators/overture_maps/install_generator.rb
@@ -1,73 +1,41 @@
 # frozen_string_literal: true
 
 require "rails/generators/base"
-require "rails/generators/migration"
 
 module OvertureMaps
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      include Rails::Generators::Migration
-
       source_root File.expand_path("../templates", __dir__)
 
       def create_migrations
         puts "Creating Overture Maps migrations..."
 
-        # Create PostGIS extension migration first
-        migration_template(
-          "postgis_migration.rb.tt",
-          "db/migrate/create_overture_postgis.rb"
-        )
+        # Get current timestamp for migration
+        timestamp = Time.now.strftime("%Y%m%d%H%M%S")
 
-        # Create categories table first (referenced by places)
-        migration_template(
-          "category_migration.rb.tt",
-          "db/migrate/create_overture_categories.rb"
-        )
+        # Create PostGIS extension migration
+        copy_file "postgis_migration.rb.tt", "db/migrate/#{timestamp}_create_overture_postgis.rb"
 
-        # Create all migrations
-        migration_template(
-          "place_migration.rb.tt",
-          "db/migrate/create_overture_places.rb"
-        )
+        # Create categories migration
+        copy_file "category_migration.rb.tt", "db/migrate/#{timestamp.succ}_create_overture_categories.rb"
 
-        migration_template(
-          "building_migration.rb.tt",
-          "db/migrate/create_overture_buildings.rb"
-        )
+        # Create places migration
+        copy_file "place_migration.rb.tt", "db/migrate/#{timestamp.succ.succ}_create_overture_places.rb"
 
-        migration_template(
-          "address_migration.rb.tt",
-          "db/migrate/create_overture_addresses.rb"
-        )
+        # Create buildings migration
+        copy_file "building_migration.rb.tt", "db/migrate/#{timestamp.succ.succ.succ}_create_overture_buildings.rb"
+
+        # Create addresses migration
+        copy_file "address_migration.rb.tt", "db/migrate/#{timestamp.succ.succ.succ.succ}_create_overture_addresses.rb"
       end
 
       def create_models
         puts "Creating Overture Maps models..."
 
-        template(
-          "category_model.rb.tt",
-          "app/models/overture_category.rb"
-        )
-
-        template(
-          "place_model.rb.tt",
-          "app/models/overture_place.rb"
-        )
-
-        template(
-          "building_model.rb.tt",
-          "app/models/overture_building.rb"
-        )
-
-        template(
-          "address_model.rb.tt",
-          "app/models/overture_address.rb"
-        )
-      end
-
-      def show_readme
-        readme "README.md" if File.exist?(destination_path("README.md"))
+        copy_file "category_model.rb.tt", "app/models/overture_category.rb"
+        copy_file "place_model.rb.tt", "app/models/overture_place.rb"
+        copy_file "building_model.rb.tt", "app/models/overture_building.rb"
+        copy_file "address_model.rb.tt", "app/models/overture_address.rb"
       end
     end
   end

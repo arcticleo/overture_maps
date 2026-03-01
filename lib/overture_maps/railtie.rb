68da24ac50c647470572d5839f6b77e151f1badc
Michael Edlund <medlund@mac.com>
2026-02-28 18:04:48 -0800

    Include rgeo

--- lib/overture_maps/railtie.rb
+++ lib/overture_maps/railtie.rb
@@ -1,8 +1,16 @@
 require "rails"
+require "rgeo/active_record"
 
 module OvertureMaps
   class Railtie < Rails::Railtie
-    initializer "overture_maps.configure" do |app|
+    initializer "overture_maps.configure" do
+      # Configure RGeo for PostGIS
+      RGeo::ActiveRecord.configure do |config|
+        config.geographic_factory_srid = 4326
+      end
+    end
+
+    initializer "overture_maps.configure_timeout" do |app|
       OvertureMaps.configure do |config|
         config.timeout = app.config.overture_maps&.timeout || 30
       end
